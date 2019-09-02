c8351b974300f4a8dc0d955bbb9d489be27c2bb9
Mental Burden <47120653+mentalburden@users.noreply.github.com>
2019-09-02 12:30:54 -0700

    Update interactive_conditional_samples.py
    Added first steps of file handling for input/output

--- src/interactive_conditional_samples.py
+++ src/interactive_conditional_samples.py
@@ -18,26 +18,7 @@ def interact_model(
     top_k=0,
     models_dir='models',
 ):
-    """
-    Interactively run the model
-    :model_name=124M : String, which model to use
-    :seed=None : Integer seed for random number generators, fix seed to reproduce
-     results
-    :nsamples=1 : Number of samples to return total
-    :batch_size=1 : Number of batches (only affects speed/memory).  Must divide nsamples.
-    :length=None : Number of tokens in generated text, if None (default), is
-     determined by model hyperparameters
-    :temperature=1 : Float value controlling randomness in boltzmann
-     distribution. Lower temperature results in less random completions. As the
-     temperature approaches zero, the model will become deterministic and
-     repetitive. Higher temperature results in more random completions.
-    :top_k=0 : Integer value controlling diversity. 1 means only 1 word is
-     considered for each step (token), resulting in deterministic completions,
-     while 40 means 40 words are considered at each step. 0 (default) is a
-     special setting meaning no restrictions. 40 generally is a good value.
-     :models_dir : path to parent folder containing model subfolders
-     (i.e. contains the <model_name> folder)
-    """
+    
     models_dir = os.path.expanduser(os.path.expandvars(models_dir))
     if batch_size is None:
         batch_size = 1
@@ -55,8 +36,8 @@ def interact_model(
 
     with tf.Session(graph=tf.Graph()) as sess:
         context = tf.placeholder(tf.int32, [batch_size, None])
-        np.random.seed(seed)
-        tf.set_random_seed(seed)
+        #np.random.seed(seed) // use pi for now
+        tf.set_random_seed(31415926)
         output = sample.sample_sequence(
             hparams=hparams, length=length,
             context=context,
@@ -69,7 +50,7 @@ def interact_model(
         saver.restore(sess, ckpt)
 
         while True:
-            raw_text = input("Model prompt >>> ")
+            raw_text = input("Pull this from a text file later, manual for now >>> ")
             while not raw_text:
                 print('Prompt should not be empty!')
                 raw_text = input("Model prompt >>> ")
@@ -84,7 +65,8 @@ def interact_model(
                     text = enc.decode(out[i])
                     print("=" * 80)
                     print(text)
-                    f = open("jangledOutput.txt", "a")
+                    #send output to file for 
+                    f = open("jangledOutput.txt", "a+")                    
                     f.write(text)
                     f.close()
             print("=" * 80)
